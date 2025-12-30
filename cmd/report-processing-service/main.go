ae58bfce6bb1f25decc87c4410f7110a89e48f6b
EmersonRabelo <rabelo.emerson1998@gmail.com>
2025-12-30 15:48:52 -0300

    feat: initializing BROKER and linking report CRUD dependencies.

--- cmd/report-processing-service/main.go
+++ cmd/report-processing-service/main.go
@@ -6,6 +6,7 @@ import (
 
 	config "github.com/EmersonRabelo/report-processing-service/internal/config"
 	database "github.com/EmersonRabelo/report-processing-service/internal/database"
+	"github.com/EmersonRabelo/report-processing-service/internal/queue"
 	router "github.com/EmersonRabelo/report-processing-service/router"
 )
 
@@ -21,9 +22,21 @@ func init() {
 
 func main() {
 	db := config.GetDB()
+
 	if err := database.RunMigrations(db); err != nil {
 		log.Fatal("Falha ao executar migrations:", err)
 	}
+
+	conn, channel := config.InitBroker()
+
+	defer channel.Close()
+	defer conn.Close()
+
+	exchange := "topic_report"
+	routingKey := "post.report.created"
+	consumer := queue.NewReportConsumer(channel, exchange, routingKey)
+
+	consumer.Consumer()
 
 	r := router.SetupRouter()
 

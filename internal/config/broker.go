e5c58dcbcde1f9702e4b80eab78bdf7232f81957
EmersonRabelo <rabelo.emerson1998@gmail.com>
2025-12-30 13:56:47 -0300

    feat: RabbitMQ connection/channel helpers and broker initializer

---
+++ internal/config/broker.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func NewRabbitMQConnection(amqpURL string) (*amqp.Connection, error) {
+	conn, err := amqp.Dial(amqpURL)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func NewRabbitMQChannel(conn *amqp.Connection) (*amqp.Channel, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+	return ch, nil
+}
+
+func InitBroker() *amqp.Channel {
+	params := AppSetting.GetBroker()
+
+	url := fmt.Sprintf(
+		"amqp://%s:%s@%s:%s/",
+		params.User,
+		params.Password,
+		params.Host,
+		params.Port,
+	)
+
+	conn, err := NewRabbitMQConnection(url)
+	if err != nil {
+		log.Fatalf("Erro ao conectar no RabbitMQ: %v", err)
+	}
+
+	defer conn.Close()
+
+	channel, err := NewRabbitMQChannel(conn)
+	if err != nil {
+		log.Fatalf("Erro ao abrir channel no RabbitMQ: %v", err)
+	}
+
+	defer channel.Close()
+
+	return channel
+}

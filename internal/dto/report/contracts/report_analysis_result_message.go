a844c78dfe31560a9252ba7fbf932d162c2749ed
EmersonRabelo <rabelo.emerson1998@gmail.com>
2026-01-07 10:48:38 -0300

    feat: new property to report struct several Toxicity and change property name from identityHate to IdentityAttack

--- internal/dto/report/contracts/report_analysis_result_message.go
+++ internal/dto/report/contracts/report_analysis_result_message.go
@@ -7,12 +7,13 @@ import (
 )
 
 type ReportAnalysisResultMessage struct {
-	ReportId     uuid.UUID  `json:"report_id"`
-	Toxicity     *float64   `json:"toxicity,omitempty"`
-	Insult       *float64   `json:"insult,omitempty"`
-	Profanity    *float64   `json:"profanity,omitempty"`
-	Threat       *float64   `json:"threat,omitempty"`
-	IdentityHate *float64   `json:"identity_hate,omitempty"`
-	Language     *string    `json:"language,omitempty"`
-	AnalyzedAt   *time.Time `json:"analyzed_at,omitempty"`
+	ReportId       uuid.UUID  `json:"report_id"`
+	Toxicity       *float64   `json:"toxicity,omitempty"`
+	SevereToxicity *float64   `json:"severe_toxicity,omitempty"`
+	IdentityAttack *float64   `json:"identity_attack,omitempty"`
+	Insult         *float64   `json:"insult,omitempty"`
+	Profanity      *float64   `json:"profanity,omitempty"`
+	Threat         *float64   `json:"threat,omitempty"`
+	Language       *string    `json:"language,omitempty"`
+	AnalyzedAt     *time.Time `json:"analyzed_at,omitempty"`
 }
--- internal/entity/report.go
+++ internal/entity/report.go
@@ -17,20 +17,21 @@ const (
 )
 
 type Report struct {
-	Id                      uuid.UUID      `gorm:"type:uuid;primaryKey;column:report_id" json:"id"`
-	PostId                  uuid.UUID      `gorm:"type:uuid;not null;column:post_id;" json:"post_id"`
-	ReporterId              uuid.UUID      `gorm:"type:uuid;not null;column:reporter_id;" json:"reporter_id"`
-	Status                  ProcessStatus  `gorm:"type:varchar(30);column:status;default:'pending'" json:"status"`
-	CreatedAt               time.Time      `gorm:"not null;default:CURRENT_TIMESTAMP;column:created_at" json:"created_at"`
-	UpdatedAt               time.Time      `gorm:"column:updated_at" json:"updated_at"`
-	DeletedAt               gorm.DeletedAt `gorm:"index;column:deleted_at" json:"deleted_at"`
-	PerspectiveToxicity     *float64       `gorm:"column:perspective_toxicity" json:"perspective_toxicity,omitempty"`
-	PerspectiveInsult       *float64       `gorm:"column:perspective_insult" json:"perspective_insult,omitempty"`
-	PerspectiveProfanity    *float64       `gorm:"column:perspective_profanity" json:"perspective_profanity,omitempty"`
-	PerspectiveThreat       *float64       `gorm:"column:perspective_threat" json:"perspective_threat,omitempty"`
-	PerspectiveIdentityHate *float64       `gorm:"column:perspective_identity_hate" json:"perspective_identity_hate,omitempty"`
-	PerspectiveLanguage     *string        `gorm:"column:perspective_language" json:"perspective_language,omitempty"`
-	PerspectiveResponseAt   *time.Time     `gorm:"column:perspective_response_at" json:"perspective_response_at,omitempty"`
+	Id                        uuid.UUID      `gorm:"type:uuid;primaryKey;column:report_id" json:"id"`
+	PostId                    uuid.UUID      `gorm:"type:uuid;not null;column:post_id;" json:"post_id"`
+	ReporterId                uuid.UUID      `gorm:"type:uuid;not null;column:reporter_id;" json:"reporter_id"`
+	Status                    ProcessStatus  `gorm:"type:varchar(30);column:status;default:'pending'" json:"status"`
+	CreatedAt                 time.Time      `gorm:"not null;default:CURRENT_TIMESTAMP;column:created_at" json:"created_at"`
+	UpdatedAt                 time.Time      `gorm:"column:updated_at" json:"updated_at"`
+	DeletedAt                 gorm.DeletedAt `gorm:"index;column:deleted_at" json:"deleted_at"`
+	PerspectiveToxicity       *float64       `gorm:"column:perspective_toxicity" json:"perspective_toxicity,omitempty"`
+	PerspectiveSevereToxicity *float64       `gorm:"column:perspective_severe_toxicity" json:"perspective_severe_toxicity,omitempty"`
+	PerspectiveInsult         *float64       `gorm:"column:perspective_insult" json:"perspective_insult,omitempty"`
+	PerspectiveProfanity      *float64       `gorm:"column:perspective_profanity" json:"perspective_profanity,omitempty"`
+	PerspectiveThreat         *float64       `gorm:"column:perspective_threat" json:"perspective_threat,omitempty"`
+	PerspectiveIdentityAttack *float64       `gorm:"column:perspective_identity_attack" json:"perspective_identity_attack,omitempty"`
+	PerspectiveLanguage       *string        `gorm:"column:perspective_language" json:"perspective_language,omitempty"`
+	PerspectiveResponseAt     *time.Time     `gorm:"column:perspective_response_at" json:"perspective_response_at,omitempty"`
 }
 
 func (report *Report) TableName() string {
